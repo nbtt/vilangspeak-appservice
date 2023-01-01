5ea8ab86d59137954c710ba7b3ca717d6a601f88
tuantnb <tuantnb@vng.com.vn>
2023-01-01 18:38:21 +0700

    Add config & change docker file to work with db

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,15 @@ FROM node:18-alpine
 
 # Bundle app source
 COPY --chown=node:node ./ /usr/src/app
+# Configuration
+COPY --chown=node:node ./src/config/config.prod.yaml /usr/src/app/src/config/config.yaml
 
 # Create app directory
 WORKDIR /usr/src/app
 
 # Install app dependencies
 ENV YARN_CACHE_FOLDER=/root/.cache/yarn
-RUN --mount=type=cache,target=/root/.cache/yarn yarn install --frozen-lockfile
+RUN --mount=type=cache,target=/root/.cache/yarn yarn install
 
 # Creates a "dist" folder with the production build
 RUN yarn build
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,31 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Category } from './entity/category.entity';
-import { Lesson } from './entity/lesson.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+
 import { LessonModule } from './lesson/lesson.module';
 import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'db',
-      port: 3306,
-      username: 'vilangspeak-user',
-      password: 'GiLJko1Tf2GfJrJl',
-      database: 'vilangspeak_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('db.host'),
+        port: +configService.get('db.port'),
+        username: configService.get('db.username'),
+        password: configService.get('db.password'),
+        database: configService.get('db.database'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     LessonModule,
     CategoryModule,
