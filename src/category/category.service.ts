c7f23087fea80023914fd91924b6cb471580ee44
tuantnb <tuantnb@vng.com.vn>
2023-01-01 22:45:13 +0700

    Add category API

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/entity/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(
+        @InjectRepository(Category)
+        private categoryRepository: Repository<Category>,
+    ) {}
+
+    getAll(): Promise<Category[]> {
+        return this.categoryRepository.find();
+    }
+}
