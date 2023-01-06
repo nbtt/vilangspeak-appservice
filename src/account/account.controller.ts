e19258574273d1b7dfd019f5b6da8499624f19ad
tuantnb <tuantnb@vng.com.vn>
2023-01-06 10:00:21 +0700

    fix account achievement add total field reponse

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -46,6 +46,7 @@ export class AccountController {
             data: {
                 account: param.id,
                 achievements: achievementsResponse,
+                total: achievementsResponse.length,
             }
         };
     }
