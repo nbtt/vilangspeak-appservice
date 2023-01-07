f17d06333eae8880ed75f8be66235a8771b39b41
tuantnb <tuantnb@vng.com.vn>
2023-01-07 11:01:15 +0700

    change progress dto add timestamp field and add test progress

--- src/dto/progress.dto.ts
+++ src/dto/progress.dto.ts
@@ -1,9 +1,26 @@
-import { IsInt, Min } from "class-validator";
+import { IsInt, IsOptional, Min } from "class-validator";
 
 export class SetLessonProgressDTO {
+    @IsOptional()
+    @IsInt()
+    public timestamp: number;
+
     @IsInt()
     @Min(0)
     public value: number;
+
+    @IsInt()
+    public account_id: number;
+}
+
+export class SetTestProgressDTO {
+    @IsOptional()
+    @IsInt()
+    public timestamp: number;
+    
+    @IsInt()
+    @Min(0)
+    public score: number;
 
     @IsInt()
     public account_id: number;
