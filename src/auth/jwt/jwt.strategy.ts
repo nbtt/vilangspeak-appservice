021aab1182a0367d31fe9b2d4ebb8ffd2a4c4d60
tuantnb <tuantnb@vng.com.vn>
2023-02-26 17:35:05 +0700

    Change service in strategy to be injected

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,11 +1,11 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(@Inject(ConfigService) configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
