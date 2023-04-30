36f4200153b18828640cb219953514f3b00ef4a4
tuantnb <tuantnb@vng.com.vn>
2023-04-30 14:00:07 +0700

    finish test category

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -3,10 +3,22 @@ import { CategoryService } from './category.service';
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  const categories = [{
+    id: 1,
+    name: 'test',
+    image: 'test',
+  }, {
+    id: 2,
+    name: 'test',
+    image: 'test',
+  }];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [CategoryService,
+      { provide: 'CategoryRepository', useValue: {
+        find: jest.fn().mockResolvedValue(categories),
+      } }],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
@@ -14,5 +26,9 @@ describe('CategoryService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get all categories', async () => {
+    expect(await service.getAll()).toEqual(categories);
   });
 });
