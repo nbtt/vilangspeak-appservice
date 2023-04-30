776c82c1422098f265c5ad12b51ded1c752e3e42
tuantnb <tuantnb@vng.com.vn>
2023-04-30 13:57:36 +0700

    unit test config

--- src/category/category.controller.spec.ts
+++ src/category/category.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/entity/category.entity';
 
 describe('CategoryController', () => {
   let controller: CategoryController;
+  const categories: Category[] = [{
+    id: 1,
+    name: 'test',
+    image: 'test',
+  }, {
+    id: 2,
+    name: 'test',
+    image: 'test',
+  }];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoryController],
+      providers: [{ provide: CategoryService, useValue: {
+        getAll: jest.fn().mockResolvedValue(categories),
+      } }],
     }).compile();
 
     controller = module.get<CategoryController>(CategoryController);
@@ -14,5 +29,15 @@ describe('CategoryController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get all categories', async () => {
+    expect(await controller.getAll()).toEqual({
+      timestamp: expect.any(Number),
+      data: {
+        categories: categories,
+        total: categories.length,
+      },
+    });
   });
 });
