6d6c58361b5fa36e78cfce8648a190fda5fd6518
tuantnb <tuantnb@vng.com.vn>
2023-05-13 03:16:11 +0700

    register add validation for username

--- src/dto/account.dto.ts
+++ src/dto/account.dto.ts
@@ -1,4 +1,6 @@
-import { IsDateString, IsOptional, IsString } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsDateString, IsOptional, IsString, Matches } from "class-validator";
+import * as _ from "lodash";
 
 export class CreateAccountDTO {
     @IsString()
@@ -7,7 +9,10 @@ export class CreateAccountDTO {
     @IsDateString({ strict: true })
     birthday: string;
 
-    @IsString()
+    @Matches(/^[a-zA-Z][a-zA-Z0-9_]{3,19}$/, {
+        message: 'Username must be 4-20 characters, start with a letter, and contain only letters, numbers, and underscores.',
+    })
+    @Transform(({value}) => _.toLower(value))
     username: string;
 
     @IsString()
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filter/http-expection.filter';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
